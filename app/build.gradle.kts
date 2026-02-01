a2b090629023ff4b0d44aacc6ae5138725386b75
FRANZISKA NEUMEISTER <info@franziska-neumeister.de>
2026-02-01 12:17:38 +0100

    refactor module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(project(":core:desingsystem"))
+    implementation(project(":core:designsystem"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,5 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "Recent Flics"
 include(":app")
-include(":core:desingsystem")
+include(":core:designsystem")
 include(":core:network")
